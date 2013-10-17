ffc38db77e526c4a496ebc87ea26229914b2e557
ryanaudiencepi <ryanaudiencepi@users.noreply.github.com>
2013-10-17 21:03:51 +0800

    Issue#1 Add Aerospike Collector

---
+++ src/collectors/aerospike/test/testaerospike.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+# coding=utf-8
+############################################################################
+
+from test import CollectorTestCase
+from test import get_collector_config
+
+from aerospike import AerospikeCollector
+
+
+class TestAerospikeCollector(CollectorTestCase):
+    def setUp(self, allowed_names=None):
+        if not allowed_names:
+            allowed_names = []
+        config = get_collector_config('AerospikeCollector', {
+        })
+        self.collector = AerospikeCollector(config, None)
+
+    def test_import(self):
+        self.assertTrue(AerospikeCollector)
